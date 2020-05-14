1e47fa0dac28e1d8613c7ef72c92c9c6bdc10aa3
ederign <ignatowicz@gmail.com>
2020-05-13 18:34:30 -0700

    Perlin noise vs random

---
+++ nature_of_code2/perlin_noise/js/sketch.js
@@ -0,0 +1,14 @@
+var xoff = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(51);
+
+  var x = map(noise(xoff), 0, 1, 0, width);
+  xoff += 0.01;
+
+  ellipse(x, 200, 24, 24);
+}
