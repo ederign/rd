41505a445c6ddfb35793e10e36ecdd9c4b96b8a7
ederign <ignatowicz@gmail.com>
2020-05-24 12:11:15 -0400

    breadth first search p2

--- noc2-il/w1/breadfirst_search/js/graph.js
+++ noc2-il/w1/breadfirst_search/js/graph.js
@@ -3,6 +3,26 @@ class Graph {
   constructor() {
     this.nodes = [];
     this.graph = {};
+    this.end = null;
+    this.start = null;
+  }
+
+  reset() {
+    this.nodes.forEach(node => {
+      node.searched = false;
+      node.parent = null;
+    });
+  }
+
+
+  setStart(actor) {
+    this.start = this.graph[actor];
+    return this.start;
+  }
+
+  setEnd(actor) {
+    this.end = this.graph[actor];
+    return this.end;
   }
 
   addNode(n) {
--- noc2-il/w1/breadfirst_search/js/sketch.js
+++ noc2-il/w1/breadfirst_search/js/sketch.js
@@ -1,5 +1,6 @@
 let data;
-var graph;
+let graph;
+let dropdown;
 
 function preload() {
   data = loadJSON('kevinbacon.json')
@@ -7,16 +8,67 @@ function preload() {
 
 function setup() {
   noCanvas();
+  dropdown = createSelect();
+  dropdown.changed(bfs)
 
   graph = new Graph();
   parseJson(data);
 
   console.log(graph);
 
+  // search("Mickey Rourke", "Mickey Rourke");
+  //search("Mickey Rourke", "Kevin Bacon");
+  // bfs("Rachel McAdams", "Kevin Bacon");
 }
 
-function draw() {
+function bfs() {
+  let queue = []
 
+  let start = graph.setStart(dropdown.value());
+  let end = graph.setEnd("Kevin Bacon");
+
+  start.searched = true;
+  queue.push(start);
+
+  while (queue.length > 0) {
+    let current = queue.shift();
+    if (current === end) {
+      console.log("Found " + current.value);
+      break;
+    }
+    var edges = current.edges;
+    edges.forEach(neighbor => {
+      if (!neighbor.searched) {
+        neighbor.searched = true;
+        neighbor.parent = current;
+        queue.push(neighbor);
+      }
+    })
+  }
+
+  printPath(end);
+  graph.reset();
+}
+
+
+function printPath(end) {
+  let path = [];
+  path.push(end);
+  let next = end.parent;
+  while (next != null) {
+    path.push(next);
+    next = next.parent;
+  }
+  var pathString = "Path: "
+  path.reverse().forEach(
+    function (node, index) {
+      pathString += node.value;
+      if ((index +1) != path.length) {
+        pathString += "-> ";
+      }
+    });
+  
+  createP(pathString);
 }
 
 function parseJson(data) {
@@ -32,6 +84,7 @@ function parseJson(data) {
       if (!actorNode) {
         actorNode = new Node(actor);
         graph.addNode(actorNode);
+        dropdown.option(actor);
       }
       movieNode.addEdge(actorNode);
     });
