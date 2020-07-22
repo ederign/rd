92cb2fa356d741b3e6d0f64a62945a07811efe7d
ederign <ignatowicz@gmail.com>
2020-07-22 15:45:27 -0400

    Story books

--- react-avancado-course/client/src/components/Main/index.tsx
+++ react-avancado-course/client/src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avancado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avancado',
+  description = 'TypeScript, ReactJS, NextJS, Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo - React Avancado escrito ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para a tela com codigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
